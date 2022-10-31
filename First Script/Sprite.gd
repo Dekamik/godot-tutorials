6f7c96127c1e9943beba8aa962131853aa310ad2
Dennis von Bargen <dennis.von-bargen@viati.se>
2022-10-31 22:28:35 +0100

    Finished Using signals

--- First Script/Sprite.gd
+++ First Script/Sprite.gd
@@ -1,19 +1,34 @@
 extends Sprite
 
+signal health_changed(old_value, new_value)
+signal health_depleted
+
+var health = 10
 var speed = 400
 var angular_speed = PI
 
+
+func _ready():
+	var timer = get_node("Timer")
+	timer.connect("timeout", self, "_on_Timer_timeout")
+
+
 func _process(delta):
-	var velocity = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		velocity = Vector2.UP.rotated(rotation) * speed
-	
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
 	position += velocity * delta
-	
-	var direction = 0
-	if Input.is_action_pressed("ui_left"):
-		direction = -1
-	if Input.is_action_pressed("ui_right"):
-		direction = 1
-		
-	rotation += angular_speed * direction * delta
+
+
+func _on_Button_pressed():
+	set_process(not is_processing())
+
+func _on_Timer_timeout():
+	visible = not visible
+
+
+func take_damage(amount):
+	var old_health = health
+	health -= amount
+	emit_signal("health_changed", old_health, health)
+	if health <= 0:
+		emit_signal("health_depleted")
